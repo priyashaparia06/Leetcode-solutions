21a42442eb8608f4c27a602f0d3daa011b9baece
priyashaparia06 <140721927+priyashaparia06@users.noreply.github.com>
2025-07-17 08:41:51 +0530

    Update and rename Solution.java to TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
