06734ca5df9f2c2cf26cf650b7e14b92796ae19c
Priya Shaparia <140721927+priyashaparia06@users.noreply.github.com>
2025-08-05 18:55:48 +0530

    26_RemoveDuplicates.cpp

---
+++ 26_RemoveDuplicates.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int a=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]!=nums[i])
+            {
+                nums[a]=nums[i];
+                a++;
+            }
+        }
+        return a;
+    }
+};
