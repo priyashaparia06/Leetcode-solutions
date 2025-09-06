b42736f7f8205a9ed05bdca96cf39e813048cd96
Priya Shaparia <140721927+priyashaparia06@users.noreply.github.com>
2025-09-06 22:56:47 +0530

    605_canPlaceFlowers.cpp

---
+++ 605_canPlaceFlowers.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int size = flowerbed.size();
+
+        for (int i = 0; i < size && n > 0; i++) {
+            if (flowerbed[i] == 0) {
+                bool emptyLeft  = (i == 0) || (flowerbed[i - 1] == 0);
+                bool emptyRight = (i == size - 1) || (flowerbed[i + 1] == 0);
+
+                if (emptyLeft && emptyRight) {
+                    flowerbed[i] = 1; // plant flower
+                    n--;              // reduce required flowers
+                }
+            }
+        }
+
+        return n == 0;
+    }
+};
